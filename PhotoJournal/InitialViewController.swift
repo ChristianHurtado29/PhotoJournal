a5daaa3264fd5974d9fc47188e09c942903cc803
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-01-25 18:57:12 -0500

    Coded in FileManagers+Extensions and Persistence Helper

---
+++ PhotoJournal/InitialViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Christian Hurtado on 1/25/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var images = [Images]()
+    
+    private let imagePickerController = UIImagePickerController()
+    
+    private let dataPersistence = PersistenceHelper(filename: "images.plist")
+    
+    private var selectedImage: UIImage?{
+    didSet{
+        appendNewPhotoToCollection()
+    }
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+private func appendNewPhotoToCollection(){
+       guard let image = selectedImage,
+           // image.jpegData(compressionQuality: 1.0) converts UIImage to Data
+           let imageData = image.jpegData(compressionQuality: 1.0) else{
+               print("image is nil")
+               return
+       }
+}
+}
--- PhotoJournal/PersistenceHelper.swift
+++ PhotoJournal/PersistenceHelper.swift
@@ -10,8 +10,97 @@ import Foundation
 
 enum DataPersistenceError: Error {
     case savingError(Error)
-    case fileDoesNotExist(Error)
+    case fileDoesNotExist(String)
     case noData
     case decodingError(Error)
     case deletingError(Error)
+}
+
+class PersistenceHelper {
+    private var events = [Images]()
+    
+    private var filename: String
+    
+    init(filename: String) {
+        self.filename = filename
+    }
+    
+    private func save() throws {
+        // step 1.
+        // get url path to the file that the Event will be saved to
+        let url = FileManager.pathToDocumentsDirectory(with: filename)
+        
+        // events array will be object being converted to Data
+        // we will use the Data object and write (save) it to documents directory
+        do {
+            // step 3.
+            // convert (serialize) the events array to Data
+            let data = try PropertyListEncoder().encode(events)
+            
+            // step 4.
+            // writes, saves, persist the data to the documents directory
+            try data.write(to: url, options: .atomic)
+        } catch {
+            // step 5.
+            throw DataPersistenceError.savingError(error)
+        }
+    }
+    
+    // for re-ordering
+    public func reorderEvents(events: [Images]) {
+        self.events = events
+        try? save()
+    }
+    
+    // DRY - don't repeat yourself
+    
+    // create - save item to documents directory
+    public func create(item: Images) throws {
+        // step 2.
+        // append new event to the events array
+        events.append(item)
+        
+        do {
+            try save()
+        } catch {
+            throw DataPersistenceError.savingError(error)
+        }
+    }
+    
+    // read - load (retrieve) items from documents directory
+    public func loadEvents() throws -> [Images] {
+        // we need access to the filename URL that we are reading from
+        let url = FileManager.pathToDocumentsDirectory(with: filename)
+        
+        // check if file exist
+        // to convert URL to String we use .path on the URL
+        if FileManager.default.fileExists(atPath: url.path) {
+            if let data = FileManager.default.contents(atPath: url.path) {
+                do {
+                    events = try PropertyListDecoder().decode([Images].self, from: data)
+                } catch {
+                    throw DataPersistenceError.decodingError(error)
+                }
+            } else {
+                throw DataPersistenceError.noData
+            }
+        }
+        else {
+            throw DataPersistenceError.fileDoesNotExist(filename)
+        }
+        return events
+    }
+    
+    // delete - remove item from documents directory
+    public func delete(event index: Int) throws {
+        // remove the item from the events array
+        events.remove(at: index)
+        
+        // save our events array to the documents directory
+        do {
+            try save()
+        } catch {
+            throw DataPersistenceError.deletingError(error)
+        }
+    }
 }
